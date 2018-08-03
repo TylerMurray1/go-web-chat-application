c9ae6569552268964d3f24c0b065945abddb8d26
TylerMurray1 <34780822+TylerMurray1@users.noreply.github.com>
2018-08-03 10:55:32 -0400

    Created files

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
+)
+
+func main() {
+	server, err :=socketio.NewServer(nil)
+	if(err != nil) {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func (so socketio.Socket) {
+		log.Println("Connection established!");
+		so.Join("chat_room")
+		so.On("chat message", func(msg string) {
+			log.Println("emit: ", so.Emit("chat message", msg))
+			so.BroadcastTo("chat_room", "chat message", msg)
+		})
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println("Serving at localhost:5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
